a64cde20d8f50e8c9c4b7c695ca008bacc8fc114
Gadzeman <ostap778800@gmail.com>
2021-05-30 16:53:50 +0300

    Sort added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,12 +2,14 @@ import React from 'react';
 import "./Products.css"
 import {useSelector} from "react-redux";
 import Product from "../Product/Product";
+import Sort from "../Sort/Sort";
 
 const Products = () => {
     const {products} = useSelector(store => store.products)
     return (
         <div>
             <h1 className={"products-title"}>Products</h1>
+            <Sort />
             <div className={"products"}>
                 {products.map(el => <Product key={el.id} product={el}/>)}
             </div>
---
+++ src/components/Sort/Sort.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Sort.css"
+
+const Sort = () => {
+    return (
+        <div>
+            <div className={"sort"}>
+                <button>Sort by COUNTER</button>
+                <button>Sort by ABC</button>
+            </div>
+        </div>
+    );
+};
+
+export default Sort;
